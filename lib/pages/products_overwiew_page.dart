d671cf2d77299d61fe064d3e3c6b80327c737b46
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2021-12-18 11:20:49 -0300

    up

--- lib/pages/products_overwiew_page.dart
+++ lib/pages/products_overwiew_page.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/providers/product_list.dart';
 import 'package:my_shop/widgets/product_grid.dart';
+import 'package:provider/provider.dart';
+
+enum FilterOptions {
+  favorite,
+  all,
+}
 
 class ProductsOverViewPage extends StatelessWidget {
   const ProductsOverViewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Shop'),
         centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            //icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.favorite,
+              ),
+              const PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.all,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
       ),
       body: const ProductGrid(),
     );
   }
 }
-
-
--- lib/providers/product_list.dart
+++ lib/providers/product_list.dart
@@ -4,8 +4,24 @@ import 'package:my_shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   List<Product> _items = dummyProducts;
+  bool _showFavoriteOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    //metodo que filtra todos os items favoritos
+    if (_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+
+    return [...items];
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+  }
 
   void addProduct(Product product) {
     _items.add(product);
